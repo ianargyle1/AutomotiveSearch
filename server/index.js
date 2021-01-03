9a67accd9854b8de7e53c87f27189b34a1c922b3
Ian <ianargyle@outlook.com>
2021-01-03 14:04:03 -0700

    discard

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
-const vehicle_api = require('../controllers/Vehicle_API_Controller');
+const vehicle_api = require('./controllers/Vehicle_API_Controller');
 const express = require('express');
+const path = require("path");
 
 const app = express();
 // const api = new Vehicle_API();
 
+const client_path = path.join(__dirname, '..', 'atomize-master');
+
+app.use(express.static(client_path));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(client_path,  'docs', 'index.html'));
 });
 
 app.get('/api/vehicles', vehicle_api.get_vehicles);

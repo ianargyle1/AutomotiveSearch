7943f38c8b1d410dfcf6971ce2352fb45d3ec498
Ian <ianargyle@outlook.com>
2021-01-03 17:45:14 -0700

    Add theme files to client

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const vehicle_api = require('./controllers/Vehicle_API_Controller');
 const express = require('express');
+const path = require('path');
 
 const app = express();
-// const api = new Vehicle_API();
+
+const client_path = path.join(__dirname, '..', 'client', 'build')
+app.use(express.static(client_path));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(client_path, 'index.html'));
 });
 
 app.get('/api/vehicles', vehicle_api.get_vehicles);
